08b5663dc287b2ac034c1ec8a1ced7b5d9b960a5
ghuali <guaghuali@gmail.com>
2025-02-07 10:00:49 +0000

    Refactorizado nombre

--- src/main/kotlin/Proyecto.kt
+++ src/main/kotlin/Proyecto.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 
-class ProyectosScreen : Screen {
+class ProyectoScreen : Screen {
     @Composable
     override fun Content() {
         var expandedTareas by remember { mutableStateOf(false) }
