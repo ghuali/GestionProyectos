b7729d9fe9904cf9b3c9c7dff401e7f14f601320
ghuali <guaghuali@gmail.com>
2025-02-09 19:43:25 +0000

    Movido tarea.kt a carpeta Screen

--- src/main/kotlin/Screen/tarea.kt
+++ src/main/kotlin/Screen/tarea.kt
@@ -1,8 +1,8 @@
+package Screen
+
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
