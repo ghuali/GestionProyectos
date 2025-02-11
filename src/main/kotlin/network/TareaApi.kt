848f9e0dfde4dcccacec53a71659cde7ab6efd05
ghuali <guaghuali@gmail.com>
2025-02-11 23:02:04 +0000

    Añadido asignarTarea

--- src/main/kotlin/network/TareaApi.kt
+++ src/main/kotlin/network/TareaApi.kt
@@ -7,6 +7,7 @@ import io.ktor.http.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import model.NuevaTarea
 import model.Tarea
 import network.NetworkUtils.httpClient
 
@@ -20,6 +21,22 @@ fun obtenerTareas(id: Int, onSuccessResponse: (List<Tarea>) -> Unit) {
         if (response.status == HttpStatusCode.OK) {
             val listTareas = response.body<List<Tarea>>()
             onSuccessResponse(listTareas)
+        } else {
+            println("Error: ${response.status}, Body: ${response.bodyAsText()}")
+        }
+    }
+}
+
+fun asignarTarea(nuevaTarea: NuevaTarea, onSuccessResponse: (Tarea) -> Unit) {
+    val url = "http://127.0.0.1:5000/proyecto/tareaProyecto"
+    CoroutineScope(Dispatchers.IO).launch {
+        val response = httpClient.post(url) {
+            contentType(ContentType.Application.Json)
+            setBody(nuevaTarea)
+        }
+        if (response.status == HttpStatusCode.OK) {
+            val tarea = response.body<Tarea>()
+            onSuccessResponse(tarea)
         } else {
             println("Error: ${response.status}, Body: ${response.bodyAsText()}")
         }
